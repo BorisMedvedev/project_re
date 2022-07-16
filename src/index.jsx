2509d03ed1f001694d22a790103ef3aa089ea084
Медведев Борис <bmedved78@mail.ru>
2022-07-16 16:05:45 +0300

    ddd

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Header } from "./Header";
+
+window.addEventListener(type:'load', listener()=>{
+  ReactDOM.render(<Header />, document.getElementById(elementid:'react_root'));
+
+});
+
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+const NODE_ENW = process.env.NODE_ENW;
+
+module.exports = {
+  mode: NODE_ENW ? NODE_ENW : 'development',
+  resolve:{
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+  },
+  entry: path.resolve(__dirname, './src/index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [{
+      test: /\.[tj]sx?$/,
+      use: ['ts-loader']
+    }]
+  }
+};
